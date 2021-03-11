43975cdeb9a4cf4460ebff068eab14562772879d
DIDEM MUMCU <mumcudidem@gmail.com>
2021-03-11 17:54:33 +0300

    110321-1754

---
+++ 01_python_giris/0115_concat.py
@@ -0,0 +1,32 @@
+#region concat 
+#değişken değiştirme
+"""
+okulNumarası = 271
+ad = "didem"
+soyad = "mumcu"
+sinavNotu = 99
+print(okulNumarası, ad, soyad, sinavNotu)
+print("okulNumarası", okulNumarası,"olan",ad, soyad,"isimli öğrencinin sınav notu", sinavNotu)
+"""
+#endregion
+
+#region concat_ornek_1
+#windows versiyonu 10 python versiyonu 3.9.2
+"""
+programlamaDili = "python"
+os = "10"
+ver = "3.9.2"
+print("windows versiyonu", os, programlamaDili, "versiyonu", ver )
+"""
+#endregion
+
+#region concat_ornek_2
+#istanbul ile danimarka arası 2800 km.
+"""
+şehir = "istanbul"
+ülke = "danimarka" 
+km = "2800"
+print(şehir, "ile",ülke, "arası",km,"km.")
+"""
+#endregion 
+
