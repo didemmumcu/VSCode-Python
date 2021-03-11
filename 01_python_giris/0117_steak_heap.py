6440ae84d43d81bdb6ce9e503b6ed0ba98605360
DIDEM MUMCU <mumcudidem@gmail.com>
2021-03-11 18:47:15 +0300

    110321-1847

---
+++ 01_python_giris/0117_steak_heap.py
@@ -0,0 +1,14 @@
+#region ram_steak_heap
+"""
+a = 1
+b = 2
+a = b
+b = 1
+print(a) #printe 2 bastı çünkü,
+#steak bölgesi sayısal veri tipleri saklanır(int, float, boolean) saklanır
+#object veri tipleri saklanır reference types saklanır
+#steak bölgesi adresleri kopyalamaz değerin kendisini kopyalar
+#steak copy,klon
+"""
+#endregion
+
--- 01_python_giris/0118_lab_pratikleri.py
+++ 01_python_giris/0118_lab_pratikleri.py
@@ -1,6 +1,55 @@
-#region vscode_ile_github_kullanimi
+# region ornek_1
 """
-adım 1 → "git enable" seçili mi?
-manage→settings→arama: "git enable" →seçili olduğuna emin olalım
+delta örneği
+"""
+a = 1
+b = 4
+c = 2
+delta = b**2 - 4*a*c
+print("delta değeri", delta)
+# endregion
 
-adım 2 → git client
+# region ornek_2
+"""
+ortalama
+"""
+a = 2
+b = 4
+ort = (a+b)/2
+print("ortalama değeri", ort)
+# endregion
+
+# region ornek_3
+"""
+saat bilgisini saniyeye dönüştürsün
+"""
+"""
+saat = 5
+saniye = 3600
+print("saat:", saat)
+print("ekrandaki saatin saniye karşılığı", saat*saniye, "sn")
+"""
+# endregion
+
+# region ipucu→ formatter shift + alt + f → bitişik yerleri ayırıyor daha okunabilir kodumuz oluyor
+
+s = 10
+x = 19
+toplam = s+x
+# endregion
+
+# region ornek_4
+""""""
+sayı = 562
+kalan = sayı % 10
+birler = kalan // 1
+print(birler)
+kalan=sayı%100
+onlar=kalan // 10
+print(onlar)
+kalan=sayı%1000
+yüzler=kalan // 100
+print(yüzler)
+toplamDegeri=birler + onlar + yüzler
+# print(yüzler, onlar, birler)
+print(toplamDegeri) 
