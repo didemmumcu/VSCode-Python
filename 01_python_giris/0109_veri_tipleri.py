d8d43d9ed5bef0e30f6e0b860ed0c78f8050c494
DIDEM MUMCU <mumcudidem@gmail.com>
2021-03-11 14:45:01 +0300

    110321-1444

---
+++ 01_python_giris/0109_veri_tipleri.py
@@ -0,0 +1,30 @@
+#region int
+#ilk veri tipimiz → integer
+print(1400) #km
+print(85) #not
+print(33) #vücut ısısı
+print(38) #vücut ısısı
+print(10000) #adım sayısı
+#endregion
+
+#region float
+#ikinci veri tipimiz → float
+print(3.14) #pi sayısı
+print(1.70) #boy
+print(0.1) #kdv değeri
+#endregion
+
+#region string
+#üçüncü veri tipimiz→string
+print("istanbul")#şehir, kitap isimleri, ders konuları, hastalık sınıflandırması
+print("ecodation")
+#endregion
+
+#region boolean
+#dördüncü veri tipimiz→boolean
+print(True)
+#lütfen true ile yazma → case sensitive
+print(False)
+#ürün satışta mı
+#endregion 
+
