1ae7ebb8eb2ac5508a86a4db92b3b7e1be1e9f14
DIDEM MUMCU <mumcudidem@gmail.com>
2021-03-11 14:51:23 +0300

    110321-1451

---
+++ 01_python_giris/0105_comment_line.py
@@ -0,0 +1,8 @@
+# comment line-yorum satırı
+"""
+1-sen bu satır/satırları görmezden gel
+2-runtime da iken bu satırı yorumlama
+3-bu bir kod değil, bu kendim için yazdığım bir hatırlatma
+"""
+#test
+#test
